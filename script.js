90af7ab2f11a1c335060e3cddb56b566b663dc38
harsh86043 <121352117+harsh86043@users.noreply.github.com>
2023-12-14 10:41:54 +0530

    "first commit"

---
+++ script.js
@@ -0,0 +1,32 @@
+if(4>3){
+    console.log("I am a good developer")
+}
+let birthday =19
+if(birthday<14){
+    console.log("birthday is passed")
+}
+else{
+    console.log("birthday is coming")
+}
+let har1=10
+console.log(har1)
+let fullNaam ="harsh"
+console.log(fullNaam)
+let brother ="shreyansh"
+console.log(brother)
+let father ="Dr. krishna Murari Pal"
+console.log(father)
+let samosa= "canteen me samosa 7 rupaye ka ho gaya hai"
+console.log(samosa)
+let chai="chai bhi ab 5 ke bajaye 7 rupaye ka ho gaya hai"
+console.log(chai)
+console.log("samosa matar ab 15 ka mil raha hai")
+console.log("litti chokha bhi 15 ka mil raha hai")
+console.log("samosa aur samosa matar ke mukable litti chokha better option hai")
+let isParampara=true
+if(isParampara){
+    console.log("lets prepare for parampara")
+}
+else{
+    console.log("padhai pe dhayan do ")
+}
